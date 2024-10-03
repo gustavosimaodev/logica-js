7cc5d42fa98fe097a7172e3a39ff5f9e96369384
Gustavo Simão | GS.dev <gs14.dev@gmail.com>
2024-10-03 12:16:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log (numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10'); //sempre testar os ERROS
 
-// se chuto for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert ('Isso aí! Você descobriu o número secreto (5)');
-} else {
-    alert ('Você errou :(')
+//let chute = prompt('Escolha um número entre 1 e 10'); //sempre testar os ERROS
+let chute;
+
+//Quantidade de tentativas - inicia em 1
+let tentativas = 1;
+
+//Enquanto - Estrutura de Looping
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // If/else - Estrutura condicional
+    if (chute == numeroSecreto) {
+        alert (`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+
+    } else {
+        if (chute < numeroSecreto) {
+            alert (`O número secreto é maior que ${chute}`);
+
+        } else {
+            alert (`O número secreto é menor que ${chute}`);
+        }
+        //Incremento: tentativas = tentativas + 1;
+        tentativas ++;
+    }
 }
+
