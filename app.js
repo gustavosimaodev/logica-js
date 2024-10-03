304e5cee830ab75665ccaa0df67374feda9a61b5
Gustavo Simão | GS.dev <gs14.dev@gmail.com>
2024-10-03 11:14:12 -0300

    Esboço do JS

---
+++ app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+console.log (numeroSecreto);
+let chute = prompt('Escolha um número entre 1 e 10'); //sempre testar os ERROS
+
+// se chuto for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert ('Isso aí! Você descobriu o número secreto (5)');
+} else {
+    alert ('Você errou :(')
+}
